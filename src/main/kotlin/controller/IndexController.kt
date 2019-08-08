33355e21b3ea9ff49427dad32cfeb1a936ddc35c
Victor <victor.schumacher@zup.com.br>
2019-08-08 16:50:25 -0300

    warning fix

--- src/main/kotlin/controller/IndexController.kt
+++ src/main/kotlin/controller/IndexController.kt
@@ -1,6 +1,5 @@
-package main.kotlin.controller
+package controller
 
-import org.slf4j.LoggerFactory
 import javax.servlet.annotation.WebServlet
 import javax.servlet.http.HttpServlet
 import javax.servlet.http.HttpServletRequest
@@ -8,10 +7,10 @@ import javax.servlet.http.HttpServletResponse
 
 @WebServlet(name = "Index", value = ["/index"])
 class IndexController: HttpServlet() {
-    var LOG = LoggerFactory.getLogger(IndexController::class.java)
+
 
     override fun doGet(req: HttpServletRequest, resp: HttpServletResponse) {
-        LOG.info("configured loggers")
+
         resp.writer.write("Hello!! Servlet application is run!")
     }
 }
