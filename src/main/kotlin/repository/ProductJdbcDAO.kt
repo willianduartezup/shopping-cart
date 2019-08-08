c7ba91c32712700a35b76972acd3bdd3679a5e65
brunoschiochetzup <bschiochet@gmail.com>
2019-08-08 10:27:19 -0300

    test unit Product DAO

--- src/main/kotlin/repository/ProductJdbcDAO.kt
+++ src/main/kotlin/repository/ProductJdbcDAO.kt
@@ -4,17 +4,17 @@ import domain.Product
 
 class ProductJdbcDAO: ProductDAO {
     override fun get(id: String): Product {
-        println("jdbc product")
+        println("jdbc product get")
         return Product("342jhj", "Apple", 2, "piece", 1)
     }
 
     override fun add(e: Product): Product {
-        println("jdbc product")
+        println("jdbc product insert")
         return e
     }
 
     override fun edit(e: Product): Product {
-        println("jdbc product")
+        println("jdbc product update")
         return e
     }
 }
--- src/test/kotlin/domain/ProductTest.kt
+++ src/test/kotlin/domain/ProductTest.kt
@@ -1,24 +1,64 @@
 package domain
 
 import infra.exception.ValidationException
+import org.junit.BeforeClass
 import org.junit.Test
+import repository.DAOFactory
+import repository.ProductDAO
+import java.util.*
 import kotlin.test.assertEquals
-import javax.validation.Validation
 
 
 class ProductTest {
+
+    companion object {
+        val id = UUID.randomUUID().toString()
+
+        @BeforeClass
+        @JvmStatic
+        fun insertProduct() {
+            println("insert......")
+            val product = Product(id, "Apple", 2, "piece", 1)
+
+            val factory = DAOFactory()
+            val productDAO: ProductDAO = factory.getInstanceOf(ProductDAO::class.java) as ProductDAO
+
+            productDAO.add(product)
+        }
+    }
+
     @Test
-    fun `product field validation`(){
+    fun `product field validation`() {
         val product = Product("", "", 0, "piece", 0)
         var ok = 0
 
         try {
             product.validateFields()
-        }catch (ex: ValidationException){
-            if(ex.listErrors.containsKey("id") && ex.listErrors.containsKey("name") && ex.listErrors.containsKey("quantity")){
+        } catch (ex: ValidationException) {
+            if (ex.listErrors.containsKey("id") && ex.listErrors.containsKey("name") && ex.listErrors.containsKey("quantity")) {
                 ok = 1
             }
         }
-        assertEquals(1,ok)
+        assertEquals(1, ok)
+    }
+
+    @Test
+    fun `validate insert product`() {
+        val factory = DAOFactory()
+        val productDAO: ProductDAO = factory.getInstanceOf(ProductDAO::class.java) as ProductDAO
+
+        assertEquals(id, productDAO.get(id).id)
+    }
+
+    @Test
+    fun `update product`() {
+        val factory = DAOFactory()
+        val productDAO: ProductDAO = factory.getInstanceOf(ProductDAO::class.java) as ProductDAO
+
+        val product = Product(id, "Orange", 2, "piece", 1)
+
+        productDAO.edit(product)
+
+        assertEquals(product.name, productDAO.get(id).name)
     }
 }
