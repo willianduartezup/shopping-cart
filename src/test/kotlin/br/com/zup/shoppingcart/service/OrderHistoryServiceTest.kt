43ed2d8b895bc88f01629da9bc469fa0e9def2cd
leonardo <email_idleonardo.filipini@zup.com.br>
2019-08-23 10:51:10 -0500

    was placed comma in the prices

---
+++ src/test/kotlin/br/com/zup/shoppingcart/service/OrderHistoryServiceTest.kt
@@ -0,0 +1,50 @@
+// br.com.zup.shoppingcart.service
+
+/*import br.com.zup.shoppingcart.ServletTestConfig
+import br.com.zup.shoppingcart.domain.SalesOrder
+import br.com.zup.shoppingcart.domain.User
+import br.com.zup.shoppingcart.repository.CartDAO
+import br.com.zup.shoppingcart.repository.SalesOrderDAO
+import br.com.zup.shoppingcart.repository.UserDAO
+import com.fasterxml.jackson.module.kotlin.convertValue
+import org.json.JSONArray
+import org.json.JSONObject
+import org.junit.Assert
+import org.junit.Test
+import java.util.ArrayList*/
+
+//class OrderHistoryServiceTest {
+
+    /*fun getOrdersUser(idUser: String): JSONArray {
+
+            return jsonArray
+
+        }catch (e: Exception) {
+            throw e
+
+    }*/
+
+   // @Test
+    //fun `should retuns the list of orders`(){
+//
+       // ServletTestConfig.LOG.info("should retuns the list of orders")
+
+        //try {
+
+           /* val jsonList = UserServiceTest.service.getListUser()
+            val users: List<User> = ServletTestConfig.mapper.convertValue(jsonList)
+
+            Assert.assertTrue(users.isNotEmpty())
+            ServletTestConfig.LOG.info("successful user return")
+
+        } catch (e: Exception) {
+
+            ServletTestConfig.LOG.error("Error testing Exception is $e")
+            Assert.assertTrue(false)
+        }
+
+    }
+
+
+
+}*/
